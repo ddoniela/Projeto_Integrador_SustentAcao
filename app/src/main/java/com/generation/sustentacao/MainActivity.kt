d05397499aab41268d4873124e2efd099e7236e1
Diego Andrade <contato.andradediego@gmail.com>
2022-06-24 11:42:48 -0300

    Adição da barra de navegação inferior

--- app/src/main/java/com/generation/sustentacao/MainActivity.kt
+++ app/src/main/java/com/generation/sustentacao/MainActivity.kt
@@ -2,13 +2,53 @@ package com.generation.sustentacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.generation.sustentacao.databinding.ActivityMainBinding
+import com.generation.sustentacao.databinding.FragmentFormBinding
+import com.generation.sustentacao.databinding.FragmentListBinding
+import com.generation.sustentacao.fragment.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val calendarFragment = CalendarFragment()
+        val chatFragment = ChatFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_calendar -> makeCurrentFragment(calendarFragment)
+                R.id.ic_chat -> makeCurrentFragment(chatFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+                R.id.ic_add -> makeCurrentFragment(addFragment)
+            }
+            true
+
+        }
+
+
+
     }
+
+
+    private fun makeCurrentFragment(fragment:Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
 }
