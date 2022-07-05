473738c25a019af1deda27ad8cccdcf1495280a4
Isabelandreoli <isabelandreoli@gmail.com>
2022-07-05 09:56:43 -0300

    Atualização de layout

--- app/src/main/java/com/generation/sustentacao/MainActivity.kt
+++ app/src/main/java/com/generation/sustentacao/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val calendarFragment = CalendarFragment()
         val chatFragment = ChatFragment()
         val settingsFragment = SettingsFragment()
+        val postagemFragment = PostagemFragment()
 
         makeCurrentFragment(homeFragment)
 
@@ -35,6 +36,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.ic_calendar -> makeCurrentFragment(calendarFragment)
                 R.id.ic_chat -> makeCurrentFragment(chatFragment)
                 R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+                R.id.ic_postagem -> makeCurrentFragment(postagemFragment)
             }
 
 
@@ -44,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeCurrentFragment(fragment:Fragment) =
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper,fragment)
+            replace(R.id.fragment_container,fragment)
             commit()
         }
 }
